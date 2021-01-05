a4f50d94539d110e4eddeff9e2bd62e6e0036f02
Magige <44432056+nkorogo@users.noreply.github.com>
2021-01-05 15:20:41 +0300

    download page

---
+++ download.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include "header.php";
+include "navheader.php";
+?>
+<!-- .nav-menu -->
+<!-- End Header -->
+
+<main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+        <div class="container">
+
+            <ol>
+                <li><a href="index.php">Home</a></li>
+            </ol>
+            <h2>Downloads</h2>
+
+        </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!--downloads-->
+    <section id="faq" class="faq section-bg">
+        <div class="container" data-aos="fade-up">
+
+            <div class="section-title">
+                <h2>Downloads Available</h2>
+            </div>
+
+            <div class="faq-list">
+                <ol>
+                    <li data-aos="fade-up" data-aos="fade-up" data-aos-delay="100">
+                        <i></i> <a data-bs-toggle="collapse" class="collapse" data-bs-target="#faq-list-1">KENGA MASSIVE SACCO BY-LAWS<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                        <div id="faq-list-1" class="collapse show" data-bs-parent=".faq-list">
+                            <p>
+                                <a href="assets/downloads/laws.pdf" download="KENGA MASSIVE SACCO BY LAWS">Download</a>
+                            </p>
+                        </div>
+                    </li>
+
+                    <li data-aos="fade-up" data-aos-delay="200">
+                        <i></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-2" class="collapsed">KENGA MEMBERSHIP APPLICATION FORM<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                        <div id="faq-list-2" class="collapse" data-bs-parent=".faq-list">
+                            <p>
+                                <a href="assets/downloads/registration.pdf" download="KENGA MEMBERSHIP APPLICATION FORM">Download</a>
+                            </p>
+                        </div>
+                    </li>
+
+                    <li data-aos="fade-up" data-aos-delay="300">
+                        <i></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-3" class="collapsed">KENGA LOAN APPLICATION FORM<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                        <div id="faq-list-3" class="collapse" data-bs-parent=".faq-list">
+                            <p>
+                                <a href="assets/downloads/loan.pdf" download="KENGA LOAN APPLICATION FORM">Download</a>
+                            </p>
+                        </div>
+                    </li>
+
+                    <li data-aos="fade-up" data-aos-delay="400">
+                        <i></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-4" class="collapsed">KENGA SIMPLE LOAN APPLICATION FORM<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                        <div id="faq-list-4" class="collapse" data-bs-parent=".faq-list">
+                            <p>
+                                <a href="assets/downloads/simpleloan.pdf" download="KENGA SIMPLE LOAN APPLICATION FORM">Download</a>
+                            </p>
+                        </div>
+                    </li
+
+                </ol>
+            </div>
+
+        </div>
+    </section>
+<!--End of downloads-->
+
+<?php
+include "footer.php";
+?>
+
+    <!-- ======= Footer ======= -->
+
+    </body>
+
+</html>
---
+++ product.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include "header.php";
+include "navheader.php";
+?>
+
+<main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+        <div class="container">
+
+            <ol>
+                <li><a href="index.php">Home</a></li>
+            </ol>
+
+        </div>
+    </section><!-- End Breadcrumbs -->
+    <h2 style="color: #0a53be; text-align: center">
+        Our range of products and services
+    </h2>
+
+    <div class="container">
+    <p>
+        <strong>
+            FOSA Services<br>
+        </strong>
+        Coming soon...
+    </p>
+    <p>
+        <strong>
+            BOSA Services<br>
+        </strong>
+        Personal/ normal loan<br>
+        Jikuze Loan (Asset Financing)<br>
+        Mshahara Loan<br>
+        Biashara  Loan<br>
+        Upesi (Emergency) Loan<br>
+        Education Loan<br>
+    </p>
+    <p>
+        <strong>
+            Our Service Centres (Points)<br>
+        </strong>
+        Kegonga  (Head Office)
+    </p>
+    <p>
+        <strong>
+            Coming soon:<br>
+        </strong>
+        Kehancha Branch<br>
+        Isebania Branch<br>
+        Mabera Branch<br>
+        Ntimaru Branch<br>
+    </p>
+    <p>
+        <strong>
+            Members Obligations and Responsibilities<br>
+        </strong>
+        we expect our Members to observe the following responsibilities to us;<br>
+        • To be courteous and respectful to our staff.<br>
+        • To be kind and polite to other customers.<br>
+        • To attend meetings (AGM & SGM) punctually.<br>
+        • To adhere to the Sacco By-laws, rules and regulations always.<br>
+        • To engage us in constructive criticism through the laid down procedures.<br>
+        • To comply with Kenyan laws in dealing with the Society.<br>
+        • To provide accurate information and authentic documents during transaction.<br>
+        • To exercise honesty and integrity in transacting with us.<br>
+        • Not to offer inducement by way of gifts and favours to Board of Directors or staff, or to solicit the same in return for our services.<br>
+    </p>
+    <p>
+        <strong>
+            Handling of Complaints and Feedback<br>
+        </strong>
+        <p>
+        Kenga Massive Sacco appreciates and encourages its members to give feedback (Both complaints and compliments).
+        We also encourage the general public to lodge genuine complaints, suggestions or compliments.
+        Our members are encouraged to submit their complaints/compliments in writing through our Website; via email to <a href="mailto:info@kengamassive.co.ke">info@kengamassive.co.ke</a> or by letter or fax to the Chairperson. Written comments can also be put in our suggestion box at any of our branches. The management shall deal with complaints professionally and in confidence. Members may be contacted through written letters, Email, telephone or face to face conversation.
+        </p>
+        Complaints will be acknowledged and assigned to a specific person to investigate and resolve. Compliments will also be appreciated.
+        Kenga Massive Sacco shall maintain complaints / suggestions register and will acknowledge receipt and act on genuine complaints within 48 hours upon receipt.
+        The above complaints mechanism does not prevent the members’ right to appeal against any response by the Society.
+        We guarantee confidentiality and privacy in respect of complainant’s identity and substance of complaint to safeguard the rights of our Members. However, we encourage complainants to identify themselves given the practical difficulties of handling anonymous grievances.
+        The Society shall periodically request our customers to evaluate our services through customer satisfaction surveys, interviews and focus groups.
+    </p>
+        <p>
+            <strong>
+                Amendment of the Charter<br>
+            </strong>
+            The Charter is subject to amendments in the light of changing organizational, socio-economic conditions or exigencies of the time. These amendments will be done with consultations with our clients and stakeholders with a view to improving our services.
+        </p>
+        <p>
+            <strong>
+                Governance<br>
+            </strong>
+            •	Annual General Meeting: This is the supreme organ charged with responsibility of making all major decisions of the Society. It is usually held once in a year.<br>
+            •	Board of Directors: The supreme body charged with the governance, control and administration of the Sacco.<br>
+            •	Chief Executive Officer (C.E.O): Chief Executive and accounting officer responsible for the effective and efficient management of the Sacco resources.<br>
+        </p>
+
+        <div class="container">
+            <h3 style="color: #0a58ca; text-align: center">
+                Our Services
+            </h3>
+            <h5 style="color: #0f5132">
+                Normal Loan
+            </h5>
+            <p>
+                This is a loan facility for all Sacco members for their business needs.
+            </p>
+
+            <h5 style="color: #0a53be">
+                Features
+            </h5>
+            <p>
+                - Loan given up to three (3) times a member’s normal shares.<br>
+                - Maximum repayment period of 36 months.
+            </p>
+            <h5 style="color: #0f5132">
+                Biashara Loan (Business loan)
+            </h5>
+            <p>
+                Loan advanced to individual group members who contribute minimum 200 shillings on weekly basis.
+            </p>
+            <h5 style="color: #0a53be">
+                Features
+            </h5>
+            <p>
+                - Flexible loan amounts to meet member needs.<br>
+                - Fast loan processing and disbursement.<br>
+                - Flexible weekly/monthly repayment up to 24 months.<br>
+                - Free two weeks Business skills training
+            </p>
+
+            <strong>
+                DEPARTMENTS
+            </strong>
+            <p>
+            Accounts and finance<br>
+            Administration department<br>
+            Credit department<br>
+            Audit department<br>
+            Marketing dept<br>
+            ICT dept
+            </p>
+
+        </div>
+
+    </div>
+
+    <?php
+    include "footer.php";
+    ?>
+
+</main>
+    <!-- ======= Footer ======= -->
+
+</body>
+
+</html>
